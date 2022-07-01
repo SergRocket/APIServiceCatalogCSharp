520e285716df64d9bacfa56138e100f6c4dcefae
SergRocket <rockettester3107@gmail.com>
2022-07-02 00:00:29 +0300

    Add project files.

---
+++ APIServiceCatalogCSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("APIServiceCatalogCSharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("APIServiceCatalogCSharp")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2a7ab29b-a7dd-4eff-9ebb-d57a4189a7a0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ APIServiceCatalogCSharp/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+
+namespace APIServiceCatalogCSharp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private object response;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var client = new RestClient("https://api2.beta.shutterfly.com");
+            var request = new RestRequest("/pipdata/v1/product/{productId}", Method.Get);
+            request.AddUrlSegment("productId", 1431302);
+            var content = client.Execute(request).Content;
+         }
+    }
+}
